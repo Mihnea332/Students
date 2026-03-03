05e4dacd3e1d319bb619bac88c2687dfb358f91e
Kaneki3332 <gabi.mihnea46@gmail.com>
2026-03-03 09:01:32 +0200

    Initial commit

---
+++ src/main/java/ro/ulbs/proiectaresoftware/students/Student.java
@@ -0,0 +1,34 @@
+package ro.ulbs.proiectaresoftware.students;
+
+public class Student {
+    private int numarMatricol;
+    String prenume;
+    String nume;
+    String formatieDeStudiu;
+
+    public int getNumarMatricol() {
+        return numarMatricol;
+    }
+
+    public String getNume() {
+        return nume;
+    }
+
+    public String getPrenume() {
+        return prenume;
+    }
+
+    public String getFormatieDeStudiu() {
+        return formatieDeStudiu;
+    }
+
+    public Student(int numarMatricol, String prenume, String nume, String formatieDeStudiu) {
+        this.numarMatricol = numarMatricol;
+        this.nume = nume;
+        this.prenume = prenume;
+        this.formatieDeStudiu = formatieDeStudiu;
+    }
+    public String toString(){
+        return String.format("%d                 %s      %s    %s",numarMatricol,prenume,nume,formatieDeStudiu);
+    }
+}
